be7dc9b8aabd8bb66f289d18c630ccd2220af319
Gian Santos <gsantos08@csu.fullerton.edu>
2022-10-12 10:02:03 -0700

    yes, let's goo

---
+++ main.py
@@ -0,0 +1,38 @@
+from weather import *
+file = "weather.dat"
+mychoice = 0
+while(True):
+    print("      *** TUFFY TITAN WEATHER LOGGER MAIN MENU")
+    print()
+    print("1. Set data filename")
+    print("2. Add weather data")
+    print("3. Print daily report")
+    print("4. Print historical report")
+    
+    print("9. Exit the program")
+    mychoice = int(input("Enter menu choice:"))
+    print()
+
+    if mychoice == 1:
+        myfile = input("Enter Data Filename:")
+        weather = read_data(filename = myfile)
+    elif mychoice == 2:
+        dt = input("Enter Date:")
+        tm = input("Enter Time: ")
+        t = int(input("Enter Temperature: "))
+        h = int(input("Enter Humidity: "))
+        r = float(input("Enter the Rainfall:"))
+        weather[dt+tm] = {'t':t, 'h':h, 'r':r}
+        write_data(data = weather, filename = myfile) 
+    elif mychoice ==3:
+        d = input("Enter date:")
+        display = report_daily(data = weather,date = d)
+        print(display)
+    elif mychoice == 4:
+        display = report_historical(weather)
+        print(display)
+    
+    elif mychoice == 9:
+        break
+
+
--- weather.py
+++ weather.py
@@ -51,21 +51,87 @@ def tot_rain(data,date):
             sum+= data[key]['r']
     return sum
 
-display = "========================= DAILY REPORT ========================"
-display += "Date                      Time  Temperature  Humidity  Rainfall"
-display += "====================  ========  ===========  ========  ======== "
-def report_daily(data,date):
-    display = "========================= DAILY REPORT ========================\n"
-    
-    for key in data:
-        if date == key[0:8]:
-            m = calendar.month_name[int(date[4:6])] + str(int(date[6:8])), "," + str(int(date[0:4]))   # month, day, year
-            tm = key[8:10] + ":" + key[10:12]+ ":" + key[12:14]
-            t = data[key]['t']
-            h = data[key]['h']
-            r = data[key]['r']
+#display = "========================= DAILY REPORT ========================"
+#display += "Date                      Time  Temperature  Humidity  Rainfall"
+#display += "====================  ========  ===========  ========  ======== "
+def report_daily(data, date):
 
-            display += f'{m:22}{tm:8}{t:13}{h:10}{r:10.2f}' + "\n"
-            return display
+    display = "========================= DAILY REPORT ========================\n"
+
+    display = (
+        display + "Date                      Time  Temperature  Humidity  Rainfall\n"
+    )
+
+    display = (
+        display + "====================  ========  ===========  ========  ========\n"
+    )
+
+    for key in data:
+
+        if date == key[0:8]:
+
+            m = (
+                calendar.month_name[int(date[4:6])]
+                + " "
+                + str(int(date[6:8]))
+                + ", "
+                + str(int(date[0:4]))
+            )
+
+            tm = key[8:10] + ":" + key[10:12] + ":" + key[12:14]
+
+            t = data[key]["t"]
+
+            h = data[key]["h"]
+
+            r = data[key]["r"]
+
+            display = display + f"{m:22}{tm:8}{t:13}{h:10}{r:10.2f}" + "\n"
+
+    return display
+
 def report_historical(data):
-    
+
+    display = (
+        "============================== HISTORICAL REPORT ===========================\n"
+    )
+    display = (
+        display
+        + "                          Minimum      Maximum   Minumum   Maximum     Total\n"
+    )
+    display = (
+        display
+        + "Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n"
+    )
+    display = (
+        display
+        + "====================  ===========  ===========  ========  ========  ========\n"
+    )
+
+    d = ""
+    for key in data:
+        if d == key[0:8]:
+            continue
+        else:
+            d = key[0:8]
+
+            m = (
+                calendar.month_name[int(d[4:6])]
+                + " "
+                + str(int(d[6:8]))
+                + ", "
+                + str(int(d[0:4]))
+            )
+
+            min_temp = min_temperature(data=data, date=d)
+            max_temp = max_temperature(data=data, date=d)
+            min_hum = min_humidity(data=data, date=d)
+            max_hum = max_humidity(data=data, date=d)
+            rain = tot_rain(data=data, date=d)
+            display = (
+                display
+                + f"{m:20}{min_temp:13}{max_temp:13}{min_hum:10}{max_hum:10}{rain:10.2f}"
+                + "\n"
+            )
+
+    return display
